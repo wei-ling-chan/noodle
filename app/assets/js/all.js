c90419e14cc6cb5373f814fc10fe3f6ae18c4aef
您的姓名 <Email>
2022-09-17 16:44:15 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,35 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+$(document).ready(function () {
+  $(".select-all").click(function () {
+      if ( $(".news-importent").hasClass('d-none') ) {
+          $(".news-importent").removeClass('d-none');
+      } 
+      if ( $(".news-newproduct").hasClass('d-none') ) {
+          $(".news-newproduct").removeClass('d-none');
+      }         
+  });
+
+  $(".select-importent").click(function () {
+      if ( $(".news-importent").hasClass('d-none') ) {
+          $(".news-importent").removeClass('d-none');
+      } 
+      if ( !($(".news-newproduct").hasClass('d-none')) ) {
+          $(".news-newproduct").addClass('d-none');
+      }         
+  });
+
+  $(".select-newproduct").click(function () {
+      if ( $(".news-newproduct").hasClass('d-none') ) {
+          $(".news-newproduct").removeClass('d-none');
+      } 
+      if ( !($(".news-importent").hasClass('d-none')) ) {
+          $(".news-importent").addClass('d-none');
+      }         
+  }); 
+
+})
+
+
